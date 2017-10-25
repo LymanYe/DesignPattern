8bdc5ea343c2977bc74f4441416cdad8108d28ed
叶立民 <lymanye@gmail.com>
2017-10-25 23:55:29 +0800

    test upload

--- proxypattern/src/main/java/com/lyman/proxypattern/base/Main.java
+++ proxypattern/src/main/java/com/lyman/proxypattern/base/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         //staticProxyMethod();
         dynamicProxyMethod();
+
     }
 
     private static void dynamicProxyMethod() {
